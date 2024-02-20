46f3aae57b31105cf66c21c9527f338f317c4483
Roberto <cell32@msn.com>
2024-02-20 16:07:57 -0600

    Added a login template and css -RM

--- src/app.py
+++ src/app.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 	
-from flask import Flask, request
+from flask import Flask, render_template, request
 	
 app = Flask(__name__)
 	
 @app.route("/")
 def main():
-    return '''
-	    <form action="/echo_user_input" method="POST">
-	        <input name="user_input">
-            <input type="submit" value="Submit!">
-	    </form>
-	     '''
+    return render_template("login.html")
+
 	
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    input_name = request.form.get("user_input", "")
+    input_password = request.form.get("user_password", "")
+    return "You entered: " + input_name, input_password
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
